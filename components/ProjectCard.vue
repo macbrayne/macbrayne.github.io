1d3257af066bae2e53c779dd60ae415100c6f05d
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2020-11-27 00:04:18 +0100

    feat: add ProjectCard image support

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,5 +1,10 @@
 <template>
   <base-card :heading="heading">
+    <img
+      v-if="imagePath"
+      :src="require('~/assets/' + imagePath)"
+      :alt="getAlt"
+    />
     <slot></slot>
     <a :href="link">{{ heading }}</a>
   </base-card>
@@ -16,6 +21,12 @@ export default {
     heading: { type: String, default: '' },
     link: { type: String, default: '' },
     description: { type: String, default: '' },
+    imagePath: { type: String, default: '' },
+  },
+  methods: {
+    getAlt() {
+      return this.heading + ' image'
+    },
   },
 }
 </script>
