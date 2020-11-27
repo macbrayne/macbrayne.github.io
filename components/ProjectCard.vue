fcc14d25b859314fef4f686574a2b6425e58b938
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2020-11-27 17:35:58 +0100

    fix: accept urls for icon location (ProjectCard.vue) fix: remove unnecessary imports

--- components/ProjectCard.vue
+++ components/ProjectCard.vue
@@ -1,22 +1,13 @@
 <template>
   <base-card :heading="heading">
-    <img
-      v-if="imagePath"
-      :src="require('~/assets/' + imagePath)"
-      :alt="getAlt"
-    />
+    <img v-if="imagePath" :src="imagePath" :alt="getAlt()" />
     <slot></slot>
     <a :href="link">{{ heading }}</a>
   </base-card>
 </template>
 
 <script>
-import BaseCard from '@/components/BaseCard'
-
 export default {
-  components: {
-    BaseCard,
-  },
   props: {
     heading: { type: String, default: '' },
     link: { type: String, default: '' },
--- layouts/default.vue
+++ layouts/default.vue
@@ -6,15 +6,3 @@
     <the-footer />
   </div>
 </template>
-
-<script>
-import TheHeader from '~/components/TheHeader.vue'
-import TheFooter from '~/components/TheFooter.vue'
-
-export default {
-  components: {
-    TheHeader,
-    TheFooter,
-  },
-}
-</script>
