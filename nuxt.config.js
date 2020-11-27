852a38e2341bc31e97cc39c1f891acb1465e25e3
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2020-11-27 18:46:59 +0100

    feat: add @nuxtjs/tailwindcss

--- nuxt.config.js
+++ nuxt.config.js
@@ -32,6 +32,8 @@ export default {
     '@nuxtjs/eslint-module',
     // https://go.nuxtjs.dev/stylelint
     '@nuxtjs/stylelint-module',
+
+    '@nuxtjs/tailwindcss',
   ],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
