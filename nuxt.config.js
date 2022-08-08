eeab268eeb21efefeebb05f7f547706c55160c9c
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2022-08-08 21:32:38 +0200

    fix: page name build: adjust dependabot intervals

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,13 +7,13 @@ export default {
     htmlAttrs: {
       lang: 'en',
     },
-    title: 'macbrayne.de',
+    title: 'Florentin Schleuß',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    // link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
