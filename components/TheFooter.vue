1f9784bee09f46f3fb51a907c61cce6f74d57136
Florentin Schleuß <27809595+macbrayne@users.noreply.github.com>
2026-02-06 15:33:04 +0100

    chore: fix footer

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,5 +1,13 @@
 <template>
   <v-footer class="site-footer" role="contentinfo">
     <nuxt-link to="/imprint">Impressum</nuxt-link>
+    <a href="https://uwu.florentin-schleuss.de">uwu</a>
   </v-footer>
 </template>
+
+<style>
+.site-footer {
+  display: flex;
+  gap: 1em;
+}
+</style>
